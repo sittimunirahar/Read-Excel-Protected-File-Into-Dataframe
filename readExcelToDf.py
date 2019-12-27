7dae6c837a0f8d0d67ef5bc87498111e5ce88813
Sitti Munirah Abdul Razak <sittimunirahar@gmail.com>
2019-12-27 12:17:22 +0800

    Update readExcelToDf.py

--- readExcelToDf.py
+++ readExcelToDf.py
@@ -4,7 +4,6 @@ import sys
 import pandas as pd
 # from xlrd import *
 
-
 xlApp = win32com.client.Dispatch("Excel.Application")
 print ("Excel library version:", xlApp.Version)
 
@@ -13,37 +12,44 @@ filename,password = r"path_to_excel_file", 'my_password'
 xlwb = xlApp.Workbooks.Open(filename, False, True, None, password)
 
 xlApp.Visible = False
-//read multiple sheets
+
+# read multiple sheets, rename sheets according to sheet name found in file
 month = ['January', "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 final_df = pd.DataFrame() 
 
-for i in month:
-    xl_sh = xlwb.Worksheets(i)
+final_df = pd.DataFrame() 
 
-    # Get last_row
-    row_num = 0
-    cell_val = ''
-    while cell_val != None:
-        row_num += 1
-        cell_val = xl_sh.Cells(row_num, 1).Value
-        # print(row_num, '|', cell_val, type(cell_val))
-    last_row = row_num - 1
-    # print(last_row)
-
-    # Get last_column
-    col_num = 0
-    cell_val = ''
-    while cell_val != None:
-        col_num += 1
-        cell_val = xl_sh.Cells(1, col_num).Value
-        # print(col_num, '|', cell_val, type(cell_val))
-    last_col = col_num - 1
-
-    content = xl_sh.Range(xl_sh.Cells(1, 1), xl_sh.Cells(last_row, last_col)).Value
-
-    df = pd.DataFrame(list(content[1:]), columns=content[0])
+for sh in xlwb.Sheets:
     
-    final_df=final_df.append(df)
+    if sh.Name in month:
+        xlwb.Worksheets(sh.Name).Activate()
+        readData = xlwb.Worksheets(sh.Name)
+        ws = xlwb.ActiveSheet
+            
+        print ('Active sheet name : ', ws.Name)
+        allData = readData.UsedRange
+
+        EndRow = allData.Rows.Count
+        EndCol = allData.Columns.Count
+        content = ws.Range(ws.Cells(1, 1), ws.Cells(EndRow, EndCol)).Value
+
+        df = pd.DataFrame(list(content))
+        
+        # set column as header
+        df.columns = df.iloc[0]
+    
+        df.drop(df.index[1])
+        
+        df.fillna(value=pd.np.nan, inplace=True)
+        #  drop null by subset 
+        df=df.dropna(subset=['ID'])
+        df=df.drop_duplicates(keep=False)
+
+        df=df[["column_1", "column_2"]]
+
+        final_df=final_df.append(df)
+    
+finaldf = final_df.reset_index(drop=True)
 
 print(final_df.head())
 xlwb.Close(False)
